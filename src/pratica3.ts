0e7146057cf7320421298d7b0b574ec0c1f06761
luizpk <luizpkpinheiro@gmail.com>
2023-02-27 13:58:16 -0300

    Resolvendo exercícios da aula

---
+++ src/pratica3.ts
@@ -0,0 +1,16 @@
+interface Pratica3{
+    soma:number,
+    mult:number
+}
+
+export const pratica3 = (x:number, y:number): Pratica3=>{
+
+    
+return{
+
+    soma: x+y,
+    mult: x*y
+}
+
+
+}
---
+++ src/pratica4.ts
@@ -0,0 +1,5 @@
+export const pratica4 = (str:string): string[] => {
+    const array=str.split('')
+
+    return array
+}
